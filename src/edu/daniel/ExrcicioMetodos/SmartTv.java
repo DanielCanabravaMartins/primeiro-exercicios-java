d090a8fb30765c3a9e20e0aeec88b5c811e75ad6
DanielCanabravaMartins <daniel.canabrava.m@gmail.com>
2023-02-06 17:54:19 -0300

    Configurando uma SmartTV

---
+++ src/edu/daniel/ExrcicioMetodos/SmartTv.java
@@ -0,0 +1,30 @@
+package edu.daniel.ExrcicioMetodos;
+
+
+public class SmartTv {
+    boolean ligada= false;
+    int canal=1;
+    int volume=25;
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+    public void mudarCanal(){
+        canal++;
+    }
+    public void ligar(){
+        ligada=true;
+    }
+    public void desligar(){
+        ligada=false;
+    }
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
+    
+    
+}
