f56e91e3185666275f1467b7e5646c3495a29d1a
DanielCanabravaMartins <daniel.canabrava.m@gmail.com>
2023-02-11 12:59:10 -0300

    Exercicios controle de fluxo

---
+++ Condicional-e-controle-de-fluxo/src/App.java
@@ -0,0 +1,27 @@
+import java.util.function.LongPredicate;
+
+public class App {
+    public static void main(String[] args)  {
+        int numero1 = 10;
+        int numero2 = 20;
+        float numero3 = 4.5f;
+        float numero4 = 3.5f;
+        char caracter1 = 'x';
+        char caracter2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Fulano";
+        String s3 = "Cicrano";
+        boolean b1 = true; 
+        boolean b2 = false;
+
+    
+        long l1 = 1597L;
+        Long l2 =8997L;
+        byte y1 = 1;
+        short h1= 25;
+        
+
+
+
+    }
+}
